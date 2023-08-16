2dd54730b53cb0a619d022cbcbf2fab968193850
Alex Meurer <alxmrer@gmail.com>
2023-08-16 12:13:02 +0200

    Initial Commit

---
+++ EvilEye/EvilEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvilEyeApp.swift
+//  EvilEye
+//
+//  Created by Alexander Meurer on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EvilEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
